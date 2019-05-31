a6f0f07ee672e605fd25dfe346009138bc466819
Alejandro Tuozzo <ale.tuozzo@gmail.com>
2019-05-30 22:36:53 -0300

    Metodos Borrar
    Se agregan métodos para borrar (incompleto).

--- Arboles/main.c
+++ Arboles/main.c
@@ -9,35 +9,54 @@ struct Nodo {
 
 struct Nodo *raiz = NULL;
 
-void insertar(int nuevoValor)
+int existeElemento(int valor)
 {
-    struct Nodo *nuevoNodo;
-
-    nuevoNodo = malloc(sizeof(struct Nodo));
-    nuevoNodo->valor = nuevoValor;
-    nuevoNodo->izq = NULL;
-    nuevoNodo->der = NULL;
-
-    if (raiz == NULL)
-        raiz = nuevoNodo;
-    else
+    struct Nodo *nodoIndice = raiz;
+    while (nodoIndice != NULL)
     {
-        struct Nodo *nodoAnterior, *nodoIndice;
-        nodoAnterior = NULL;
-        nodoIndice = raiz;
-
-        while (nodoIndice != NULL)
-        {
-            nodoAnterior = nodoIndice;
-            if (nuevoValor < nodoIndice->valor)
-                nodoIndice = nodoIndice->izq;
-            else
-                nodoIndice = nodoIndice->der;
-        }
-        if (nuevoValor < nodoAnterior->valor)
-            nodoAnterior->izq = nuevoNodo;
+        if (valor == nodoIndice->valor)
+                return 1;
         else
-            nodoAnterior->der = nuevoNodo;
+            if (valor > nodoIndice->valor)
+                nodoIndice = nodoIndice->der;
+            else
+                nodoIndice = nodoIndice->izq;
+    }
+    return 0;
+}
+
+void insertar(int valor)
+{
+    if (!existeElemento(valor))
+    {
+        struct Nodo *nuevoNodo;
+
+        nuevoNodo = malloc(sizeof(struct Nodo));
+        nuevoNodo->valor = valor;
+        nuevoNodo->izq = NULL;
+        nuevoNodo->der = NULL;
+
+        if (raiz == NULL)
+            raiz = nuevoNodo;
+        else
+        {
+            struct Nodo *nodoAnterior, *nodoIndice;
+            nodoAnterior = NULL;
+            nodoIndice = raiz;
+
+            while (nodoIndice != NULL)
+            {
+                nodoAnterior = nodoIndice;
+                if (valor < nodoIndice->valor)
+                    nodoIndice = nodoIndice->izq;
+                else
+                    nodoIndice = nodoIndice->der;
+            }
+            if (valor < nodoAnterior->valor)
+                nodoAnterior->izq = nuevoNodo;
+            else
+                nodoAnterior->der = nuevoNodo;
+        }
     }
 }
 
@@ -45,7 +64,7 @@ void recorrerPreOrden(struct Nodo *nodoIndice)
 {
     if (nodoIndice != NULL)
     {
-        printf("%i-",nodoIndice->valor);
+        printf("%i-", nodoIndice->valor);
         recorrerPreOrden(nodoIndice->izq);
         recorrerPreOrden(nodoIndice->der);
     }
@@ -56,7 +75,7 @@ void recorrerInOrden(struct Nodo *nodoIndice)
     if (nodoIndice != NULL)
     {
         recorrerInOrden(nodoIndice->izq);
-        printf("%i-",nodoIndice->valor);
+        printf("%i-", nodoIndice->valor);
         recorrerInOrden(nodoIndice->der);
     }
 }
@@ -67,9 +86,66 @@ void recorrerPostOrden(struct Nodo *nodoIndice)
     {
         recorrerPostOrden(nodoIndice->izq);
         recorrerPostOrden(nodoIndice->der);
-        printf("%i-",nodoIndice->valor);
+        printf("%i-", nodoIndice->valor);
     }
 }
+
+void borrar(struct Nodo *nodoIndice)
+{
+    if (nodoIndice != NULL)
+    {
+        borrar(nodoIndice->izq);
+        borrar(nodoIndice->der);
+        free(nodoIndice);
+    }
+}
+
+void borrarMinimo(struct Nodo *raiz)
+{
+     if (raiz != NULL) {
+         struct Nodo *nodoBorrar;
+         if (raiz->izq == NULL)
+         {
+             nodoBorrar = raiz;
+             raiz = raiz->der;
+             free(nodoBorrar);
+         }
+         else {
+             struct Nodo *nodoAnterior = raiz;
+             struct Nodo *nodoIndice = raiz->izq;
+             while (nodoIndice->izq != NULL)
+             {
+                 nodoAnterior = nodoIndice;
+                 nodoIndice = nodoIndice->izq;
+             }
+             nodoAnterior->izq = nodoIndice->der;
+             free(nodoIndice);
+         }
+     }
+ }
+
+/*struct Nodo BorrarElemento(int elemento, struct Nodo *raiz)
+{
+	//if(raiz == NULL)
+	//	throw new ItemNotFoundException(x.toString());
+
+	if (elemento < raiz->valor)
+		raiz->izq = BorrarElemento(elemento, raiz->izq);
+	else if(elemento > raiz->valor)
+		raiz->der = BorrarElemento(elemento, raiz->der);
+	else if(raiz->izq != NULL && raiz->der != NULL) //Nodo con dos hijos
+	{
+		raiz->valor = buscarMinimo(raiz->der)->valor;
+		raiz->der = borrarMinimo(raiz->der);
+	}
+	else
+		if (raiz->izq != NULL)
+            raiz = raiz->izq;
+		else
+            raiz = raiz->der;
+
+	return raiz;
+}*/
 
 int main()
 {
